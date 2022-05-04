163ff4b0688cf304ca57a4b976e54745f311e6b0
Livia costa da silva <livia.silva@conecthus.org.br>
2022-05-04 18:01:58 -0400

    ...

--- src/components/Widget.tsx
+++ src/components/Widget.tsx
@@ -1,24 +1,23 @@
 import { ChatTeardropDots } from "phosphor-react";
 import { useState } from "react";
-
-
+import { Popover } from "@headlessui/react";
+import { WidgetForm } from "./WidgetForm";
 
 export function Widget() {
-    const [isWidgetOpen, setIsWidgetOpen] = useState(false)
-  
-    return (
-    <div className="absolute bottom-5 right-5">
-        <p>Hello!</p>
-      <button className="bg-brand-500 rounded-full 
-      text-white px-3 h-12 flex items-center group">
+  return (
+    <Popover className="absolute bottom-5 right-5 flex-col items-end">
+      <Popover.Panel>Hello!</Popover.Panel>
+      <WidgetForm/>
+      <Popover.Button
+        className="bg-brand-500 rounded-full 
+      text-white px-3 h-12 flex items-center group"
+      >
         <ChatTeardropDots className="w-6 h-6 " weight="regular" />
 
         <span className="max-w-0 overflow-hidden group-hover:max-w-xs transition-all duration-500 ease-linear">
-            <span className="pl-2">
-            Feedback
-            </span>
-            </span>
-      </button>
-    </div>
+          <span className="pl-2">Feedback</span>
+        </span>
+      </Popover.Button>
+    </Popover>
   );
 }
